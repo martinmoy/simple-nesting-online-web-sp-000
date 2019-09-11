337e200859d601fdae3e6496341e6f0218353621
Martin Moy <martinmoy1991@gmail.com>
2019-09-11 07:32:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,11 +84,12 @@ def adding_matz
         }
      }
 
-		 programmer_hash.merge{
+		 programmer_hash.merge {
 			 :yukihiro_matsumoto => {
 			 :known_for => "Ruby",
 			 :languages => ["LISP", "C"]}
 		 }
+	 end
 
 end
 
